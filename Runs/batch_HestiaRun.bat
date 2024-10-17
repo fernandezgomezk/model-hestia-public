af2cb1deb75ac3dffc40555d0c1599c523db3cb1
teunistj <thije.teunis@tno.nl>
2024-10-17 16:34:12 +0200

    voortgang 10/17/24

--- Runs/batch_HestiaRun.bat
+++ Runs/batch_HestiaRun.bat
@@ -5,8 +5,11 @@ REM Resultaten worden weggeschreven in: n:/LD/%confignaam%/results/...
 REM Bij nogmaals runnen worden bestanden gewoon overschreven
 
 REM aanpassingen voor machine specifieke paden
+
 Call path/set.bat
 
-"%exe_path%" "/L%log_dir%\HestiaRun.txt"  "%prj_dir%\Runs\HestiaRun.dms" /Resultaten/Exports/GenerateAllIndicatoren
+ 
+START "HESTIA" /WAIT /BELOWNORMAL "%exe_path%" "/L%log_dir%\HestiaRun.txt"  "%prj_dir%\Runs\HestiaRun.dms" /TussenResultaten/StartJaar/AllocatieResultaten/Woning/BO/Resultaten_Floris/export_csv/Export
+COPY /b/v/y "C:\Users\teunistj\Documents\Results\model-hestia-public\Results\stage_floris\StartJaar_Resulaten_Floris_export.csv" 
 
 pause Totaal klaar
