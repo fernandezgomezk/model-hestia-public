af14d3d72060d96602693d9fa72516b412397c02
teunistj <thije.teunis@tno.nl>
2024-11-07 17:19:48 +0100

    minimum energielabel veranderd naar minimum schillabel

--- Runs/batch_HestiaRun.bat
+++ Runs/batch_HestiaRun.bat
@@ -7,6 +7,8 @@ REM Bij nogmaals runnen worden bestanden gewoon overschreven
 REM aanpassingen voor machine specifieke paden
 Call path/set.bat
 
-"%exe_path%" "/L%log_dir%\HestiaRun.txt"  "%prj_dir%\Runs\HestiaRun.dms" /Resultaten/Exports/GenerateAllIndicatoren
+START "HESTIA" /WAIT /BELOWNORMAL "%exe_path%" "/L%log_dir%\HestiaRun.txt"  "%prj_dir%\Runs\HestiaRun_s1a.dms" /TussenResultaten/Startjaar/AllocatieResultaten/Woning/BO/Resultaten_warmtepompgegevens/export_csv/Export
+
+START "HESTIA" /WAIT /BELOWNORMAL "%exe_path%" "/L%log_dir%\HestiaRun.txt"  "%prj_dir%\Runs\HestiaRun_s1b.dms" /TussenResultaten/Startjaar/AllocatieResultaten/Woning/BO/Resultaten_warmtepompgegevens/export_csv/Export
 
 pause Totaal klaar
