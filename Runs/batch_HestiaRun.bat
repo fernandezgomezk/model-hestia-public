597748209e4878ddef8b517a468e65f50d753573
teunistj <thije.teunis@tno.nl>
2025-04-10 16:56:18 +0200

    opzetten batchbestanden

--- Runs/batch_HestiaRun.bat
+++ Runs/batch_HestiaRun.bat
@@ -7,7 +7,21 @@ REM Bij nogmaals runnen worden bestanden gewoon overschreven
 REM aanpassingen voor machine specifieke paden
 Call path/set.bat
 
-START "HESTIA" /WAIT /BELOWNORMAL "%exe_path%" "/L%log_dir%\HestiaRun.txt"  "%prj_dir%\Runs\HestiaRun.dms" /TussenResultaten/R30_2050/AllocatieResultaten/Woning/BO/Outputtabellen/export_csv/Export
+START "HESTIA" /WAIT /BELOWNORMAL "%exe_path%" "/L%log_dir%\HestiaRun.txt"  "%prj_dir%\Runs\HestiaRun.dms" /TussenResultaten/StartJaar/AllocatieResultaten/Woning/BO/Outputtabellen/export_csv/Export
+START "HESTIA" /WAIT /BELOWNORMAL "%exe_path%" "/L%log_dir%\HestiaRun.txt" "%prj_dir%\Runs\HestiaRun.dms" /TussenResultaten/R1_2021/AllocatieResultaten/Woning/BO/Outputtabellen/export_csv/Export
+START "HESTIA" /WAIT /BELOWNORMAL "%exe_path%" "/L%log_dir%\HestiaRun.txt" "%prj_dir%\Runs\HestiaRun.dms" /TussenResultaten/R2_2022/AllocatieResultaten/Woning/BO/Outputtabellen/export_csv/Export
+START "HESTIA" /WAIT /BELOWNORMAL "%exe_path%" "/L%log_dir%\HestiaRun.txt" "%prj_dir%\Runs\HestiaRun.dms" /TussenResultaten/R3_2023/AllocatieResultaten/Woning/BO/Outputtabellen/export_csv/Export
+START "HESTIA" /WAIT /BELOWNORMAL "%exe_path%" "/L%log_dir%\HestiaRun.txt" "%prj_dir%\Runs\HestiaRun.dms" /TussenResultaten/R4_2024/AllocatieResultaten/Woning/BO/Outputtabellen/export_csv/Export
+START "HESTIA" /WAIT /BELOWNORMAL "%exe_path%" "/L%log_dir%\HestiaRun.txt" "%prj_dir%\Runs\HestiaRun.dms" /TussenResultaten/R5_2025/AllocatieResultaten/Woning/BO/Outputtabellen/export_csv/Export
+START "HESTIA" /WAIT /BELOWNORMAL "%exe_path%" "/L%log_dir%\HestiaRun.txt" "%prj_dir%\Runs\HestiaRun.dms" /TussenResultaten/R6_2026/AllocatieResultaten/Woning/BO/Outputtabellen/export_csv/Export
+START "HESTIA" /WAIT /BELOWNORMAL "%exe_path%" "/L%log_dir%\HestiaRun.txt" "%prj_dir%\Runs\HestiaRun.dms" /TussenResultaten/R7_2027/AllocatieResultaten/Woning/BO/Outputtabellen/export_csv/Export
+START "HESTIA" /WAIT /BELOWNORMAL "%exe_path%" "/L%log_dir%\HestiaRun.txt" "%prj_dir%\Runs\HestiaRun.dms" /TussenResultaten/R8_2028/AllocatieResultaten/Woning/BO/Outputtabellen/export_csv/Export
+START "HESTIA" /WAIT /BELOWNORMAL "%exe_path%" "/L%log_dir%\HestiaRun.txt" "%prj_dir%\Runs\HestiaRun.dms" /TussenResultaten/R9_2029/AllocatieResultaten/Woning/BO/Outputtabellen/export_csv/Export
+START "HESTIA" /WAIT /BELOWNORMAL "%exe_path%" "/L%log_dir%\HestiaRun.txt" "%prj_dir%\Runs\HestiaRun.dms" /TussenResultaten/R10_2030/AllocatieResultaten/Woning/BO/Outputtabellen/export_csv/Export
+START "HESTIA" /WAIT /BELOWNORMAL "%exe_path%" "/L%log_dir%\HestiaRun.txt" "%prj_dir%\Runs\HestiaRun.dms" /TussenResultaten/R15_2035/AllocatieResultaten/Woning/BO/Outputtabellen/export_csv/Export
+START "HESTIA" /WAIT /BELOWNORMAL "%exe_path%" "/L%log_dir%\HestiaRun.txt" "%prj_dir%\Runs\HestiaRun.dms" /TussenResultaten/R20_2040/AllocatieResultaten/Woning/BO/Outputtabellen/export_csv/Export
+START "HESTIA" /WAIT /BELOWNORMAL "%exe_path%" "/L%log_dir%\HestiaRun.txt" "%prj_dir%\Runs\HestiaRun.dms" /TussenResultaten/R25_2045/AllocatieResultaten/Woning/BO/Outputtabellen/export_csv/Export
+START "HESTIA" /WAIT /BELOWNORMAL "%exe_path%" "/L%log_dir%\HestiaRun.txt" "%prj_dir%\Runs\HestiaRun.dms" /TussenResultaten/R30_2050/AllocatieResultaten/Woning/BO/Outputtabellen/export_csv/Export
 
 
 pause Totaal klaar
--- Runs/path/set.bat
+++ Runs/path/set.bat
@@ -1,8 +1,8 @@
 REM aanpassingen voor machine specifieke paden
 
-set prj_dir=C:\prj\pbl\SawecPerfTest
-set log_dir=%prj_dir%\log
-set exe_dir=C:\Program Files\ObjectVision\GeoDms8032
+set prj_dir=C:\Users\teunistj\Documents\KEV2025\model-hestia-development-kev_nieuw
+set log_dir=C:\Users\teunistj\Documents\Results\log
+set exe_dir=C:\Program Files\ObjectVision\GeoDms16.0.0
 
 REM einde aanpassingen voor machine specifieke paden
 
