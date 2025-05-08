90225400df72e171273daf48feb5990b86033444
MaartenHilferink <mhilferink@objectvision.nl>
2025-05-08 14:59:27 +0200

    set path to OVSRV10

--- Runs/path/set.bat
+++ Runs/path/set.bat
@@ -1,8 +1,8 @@
 REM aanpassingen voor machine specifieke paden
 
-set prj_dir=C:\ProjDir_Maarten\model-hestia-public-kfg
+set prj_dir=E:\prj\TNO\model-hestia-public
 set log_dir=C:\LocalData\Hestia\log
-set exe_dir=C:\Program Files\ObjectVision\GeoDms17.3.1
+set exe_dir=C:\Program Files\ObjectVision\GeoDms17.4.6
 
 REM einde aanpassingen voor machine specifieke paden
 set exe_path=%exe_dir%\GeoDMSRun.exe
