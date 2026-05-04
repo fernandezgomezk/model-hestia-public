6e430f37b84137c5ad386ac6bf1f33c2adf71dbd
teunistj <thije.teunis@tno.nl>
2026-05-04 09:48:09 +0200

    aanpassingen voor run 1 mei 2026

--- Runs/path/set.bat
+++ Runs/path/set.bat
@@ -1,6 +1,6 @@
 REM aanpassingen voor machine specifieke paden
 
-set prj_dir=C:\Users\teunistj\Documents\Eindgebruikerskosten\model-hestia-public
+set prj_dir=C:\Users\teunistj\Documents\investeringskosten_wi
 set log_dir=C:\Users\teunistj\Documents\Results\log
 set exe_dir=C:\Program Files\ObjectVision\GeoDms16.0.3
 
